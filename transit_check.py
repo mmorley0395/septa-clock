303f9edb9989b732c589fbfbcb90443adef1efea
mmorley <mmorley0395@gmail.com>
2022-08-09 22:06:28 -0400

    fixed incorrect datetime

--- transit_check.py
+++ transit_check.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime, timedelta, timezone
+
 from RPLCD.i2c import CharLCD
 
 lcd = CharLCD("PCF8574", 0x27)
@@ -15,8 +16,8 @@ next_three_busses = r.get(f"{bus_num}")
 next_bus = next_three_busses[0]
 now = datetime.now()
 next_bus_time = next_bus["DateCalender"]
-datetime_object = datetime.strptime(next_bus_time, "%d/%m/%y %I:%M %p")
+datetime_object = datetime.strptime(next_bus_time, "%m/%d/%y %I:%M %p")
 arrives_in = datetime_object - now
 minutes = str(arrives_in)[:4]
-
+print(minutes)
 lcd.write_string(f"the {bus_num} comes\n\rin {minutes} minutes")
